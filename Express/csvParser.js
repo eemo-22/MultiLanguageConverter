14d42affc1692a63ff71391b35dd9a10f8e2036a
강호진 <eemo22@natoo.co>
2022-10-18 19:49:39 +0900

    기본적인 기계번역 구성 완료

--- Express/csvParser.js
+++ Express/csvParser.js
@@ -3,23 +3,29 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "1";
 
 const path = require("path");
 const fs = require("fs");
-const FILE_NAME = "natooLang.csv";
-const csvPath = path.join(__dirname, '..', 'Express/csv', FILE_NAME);
+const { join } = require('path');
+
+const SAVE_FILE_NAME = "resource.csv";
+const resourcePath = path.join(__dirname, '..', 'Express/result', SAVE_FILE_NAME);
+
+const RESULT_NAME = "multilingualResource.tsv";
+const resultPath = path.join(__dirname, '..', 'Express/result', RESULT_NAME);
 // console.log(csvPath);
 
-const csv = fs.readFileSync(csvPath, "utf-8");
+const csv = fs.readFileSync(resourcePath, "utf-8");
 
-const rows = csv.split("\n");
-console.log(rows);
+//  get string from csv
+const rows = csv.split("\r\n");
+// console.log('rows', rows);
 
 let results = [];
 let columnTitle = [];
 for (const i in rows) {
   const row = rows[i];
-  const data = row.split(",");
+  const data = row.split("\t");
   if (i === "0") {
     columnTitle = data;
-    console.log(columnTitle);
+    // console.log('columnTitle', columnTitle);
   } else {
     let row_data = {};
     for (const index in columnTitle) {
@@ -29,17 +35,17 @@ for (const i in rows) {
     results.push(row_data);
   }
 }
-// console.log(results);
-
+//  get Korean resources
 const resource = [];
 for (let i in results) {
   const korean = results[i].ko;
   resource.push(korean);
 }
-console.log(resource);
+console.log('resource', resource);
 
+//  translation
 // Imports the Google Cloud client library
-const {Translate} = require('@google-cloud/translate').v2;
+const { Translate } = require('@google-cloud/translate').v2;
 
 // Creates a client
 const translate = new Translate();
@@ -47,24 +53,38 @@ const translate = new Translate();
 const text = resource;
 const target1 = 'en';
 const target2 = 'zh';
+const enTranslated = [];
+const zhTranslated = [];
 
 async function translateEn() {
   let [translations] = await translate.translate(text, target1);
   translations = Array.isArray(translations) ? translations : [translations];
-  console.log('Translations:');
+  console.log('en Translations:');
   translations.forEach((translation, i) => {
     console.log(`${i}, ${text[i]} => (${target1}) ${translation}`);
+    enTranslated.push(translations[i]);
   });
-}
+  translateZh();
+} translateEn();
+
 async function translateZh() {
   let [translations] = await translate.translate(text, target2);
   translations = Array.isArray(translations) ? translations : [translations];
-  console.log('Translations:');
+  console.log('zh Translations:');
   translations.forEach((translation, i) => {
     console.log(`${i}, ${text[i]} => (${target2}) ${translation}`);
+    zhTranslated.push(translations[i]);
   });
-}
+  fileWriter();
+};
 
-translateEn();
-translateZh();
+function fileWriter() {
 
+  let stringSum = '';
+  for (let i = 0; i < text.length; i++) {
+    stringSum += text[i] + '\t' + enTranslated[i] + '\t' + zhTranslated[i] + '\n';
+  }
+
+  fs.writeFileSync(resultPath, stringSum)
+  console.log('success!');
+};
--- Express/index.js
+++ Express/index.js
@@ -1,8 +1,15 @@
 require('dotenv').config();
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "1";
 const express = require('express');
+
 const app = express();
 const port = 3000;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Translation app listening on port ${port}`);
+})
 
 const path = require("path");
 const fs = require("fs");
@@ -12,75 +19,15 @@ const csv = fs.readFileSync(csvPath, "utf-8");
 
 // const translateRouter = require("./routes/translateRouter");
 
-const { Translate } = require('@google-cloud/translate').v2;
-const translate = new Translate();
+// const { Translate } = require('@google-cloud/translate').v2;
+// const translate = new Translate();
 
 // app.use('/api/translate', translateRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 
-  const rows = csv.split("\r\n");
-  let results = [];
-  let columnTitle = [];
-  for (const i in rows) {
-    const row = rows[i];
-    const data = row.split(",");
-    if (i === "0") {
-      columnTitle = data;
-    } else {
-      let row_data = {};
-      for (const index in columnTitle) {
-        const title = columnTitle[index];
-        row_data[title] = data[index];
-      }
-      results.push(row_data);
-    }
-  }
-  // console.log(results[0].ko);
+  console.log(csv);
 
-  const resource = [];
-  for (let i in results) {
-    const korean = results[i].ko;
-    resource.push(korean);
-  }
-  console.log(resource);
-  console.log(Array.isArray(resource));
-
-  const txtHeader = ['ko', 'en', 'zh'];
-const csvWriterHeader = txtHeader.map((el) => {
-  return {
-    id: el,
-    title: el
-  };
-});
-
-let result = [];
-for (let i = 0; i < 10; i++) {
-  let data = {
-    ko: csv.ko,
-    en: csv.en,
-    zh: csv.zh,
-  };
-  result.push(data);
-}
-
-console.log(result);
-
-  // const text = resource;
-  // const target = 'en';
-
-  // async function translateText() {
-  //   let [translations] = await translate.translate(text, target);
-  //   translations = Array.isArray(translations) ? translations : [translations];
-  //   console.log('Translations:');
-  //   translations.forEach((translation, i) => {
-  //     console.log(`${text[i]} => (${target}) ${translation}`);
-  //   });
-  // }
-  // translateText();
 })
 
-app.listen(port, () => {
-  console.log(`Translation app listening on port ${port}`);
-})
