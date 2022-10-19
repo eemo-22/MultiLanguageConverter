89df44c509b4997f27debc65200ae0883228dcbc
강호진 <eemo22@natoo.co>
2022-10-19 19:55:46 +0900

    128개 이상에서 api호출 처리

--- Express/csvParser.js
+++ Express/csvParser.js
@@ -5,11 +5,13 @@ const path = require("path");
 const fs = require("fs");
 const { join } = require('path');
 
-const SAVE_FILE_NAME = "resource.csv";
-const resourcePath = path.join(__dirname, '..', 'Express/result', SAVE_FILE_NAME);
+const RESOURCE_FILE_NAME = "convert01.tsv";
+const resourcePath = path.join(__dirname, '..', 'Express/result', RESOURCE_FILE_NAME);
 
-const RESULT_NAME = "multilingualResource.tsv";
-const resultPath = path.join(__dirname, '..', 'Express/result', RESULT_NAME);
+const EN_RESULT_NAME = "en-multilingualResource.tsv";
+const enResultPath = path.join(__dirname, '..', 'Express/result', EN_RESULT_NAME);
+const ZH_RESULT_NAME = "zh-multilingualResource.tsv";
+const zhResultPath = path.join(__dirname, '..', 'Express/result', ZH_RESULT_NAME);
 // console.log(csvPath);
 
 const csv = fs.readFileSync(resourcePath, "utf-8");
@@ -18,30 +20,19 @@ const csv = fs.readFileSync(resourcePath, "utf-8");
 const rows = csv.split("\r\n");
 // console.log('rows', rows);
 
-let results = [];
-let columnTitle = [];
+// let results = [];
+const resource = [];
+
 for (const i in rows) {
   const row = rows[i];
   const data = row.split("\t");
-  if (i === "0") {
-    columnTitle = data;
-    // console.log('columnTitle', columnTitle);
-  } else {
-    let row_data = {};
-    for (const index in columnTitle) {
-      const title = columnTitle[index];
-      row_data[title] = data[index];
-    }
-    results.push(row_data);
-  }
+  resource.push(data[0]);
 }
-//  get Korean resources
-const resource = [];
-for (let i in results) {
-  const korean = results[i].ko;
-  resource.push(korean);
-}
+
+
+let resourceSet = Array.from(new Set(resource));
 console.log('resource', resource);
+console.log('resourceSet', resourceSet);
 
 //  translation
 // Imports the Google Cloud client library
@@ -50,41 +41,90 @@ const { Translate } = require('@google-cloud/translate').v2;
 // Creates a client
 const translate = new Translate();
 
-const text = resource;
+
+//  google api v2는 한 번에 128세그먼트까지 요청을 받는다
+//  resourceSet의 길이가 128 초과인 경우, 분할해 요청할 필요 있음 
+
+
+const text = resourceSet;
+const textBuff = text;
+let bigText = [];
+
+
+console.log('text', textBuff)
+
 const target1 = 'en';
 const target2 = 'zh';
 const enTranslated = [];
 const zhTranslated = [];
 
 async function translateEn() {
-  let [translations] = await translate.translate(text, target1);
+  bigText = [];
+  if (textBuff.length > 128) {
+    for (let i = 0; i < 128; i++) {
+      console.log('shift');
+      bigText.push(textBuff.shift());
+    }
+  } else {
+    console.log('ok');
+    for (let i = 0; i < textBuff.length; i++) {
+      bigText.push(textBuff.shift());
+    }
+    console.log('how many?', bigText.length);
+  }
+
+  let [translations] = await translate.translate(bigText, target1);
   translations = Array.isArray(translations) ? translations : [translations];
   console.log('en Translations:');
   translations.forEach((translation, i) => {
-    console.log(`${i}, ${text[i]} => (${target1}) ${translation}`);
+    console.log(`${i}, ${bigText[i]} => (${target1}) ${translation}`);
     enTranslated.push(translations[i]);
   });
   translateZh();
-} translateEn();
+}translateEn();
 
 async function translateZh() {
-  let [translations] = await translate.translate(text, target2);
+  let [translations] = await translate.translate(bigText, target2);
   translations = Array.isArray(translations) ? translations : [translations];
   console.log('zh Translations:');
   translations.forEach((translation, i) => {
-    console.log(`${i}, ${text[i]} => (${target2}) ${translation}`);
+    console.log(`${i}, ${bigText[i]} => (${target2}) ${translation}`);
     zhTranslated.push(translations[i]);
   });
-  fileWriter();
+  if (textBuff.length === 0) {
+    fileWriter();
+  } else {
+    translateEn();
+  }
 };
 
 function fileWriter() {
+  console.log('file writing');
+  let enStringSum = '';
+  resource.forEach(item => {
+    // console.log('item', item);
+    let idx = resource.findIndex((textItem) => textItem === item);
+    // stringSum += text[idx] + '\t' + enTranslated[idx] + '\t' + zhTranslated[idx] + '\n';
+    if (resource.length === idx + 1) {
+      enStringSum += enTranslated[idx];
+    } else {
+      enStringSum += enTranslated[idx] + '\n';
+    }
+    fs.writeFileSync(enResultPath, enStringSum)
+  })
 
-  let stringSum = '';
-  for (let i = 0; i < text.length; i++) {
-    stringSum += text[i] + '\t' + enTranslated[i] + '\t' + zhTranslated[i] + '\n';
-  }
+  let zhStringSum = '';
+  resource.forEach(item => {
+    // console.log('item', item);
+    let idx = resource.findIndex((textItem) => textItem === item);
+    // stringSum += text[idx] + '\t' + enTranslated[idx] + '\t' + zhTranslated[idx] + '\n';
+    if (resource.length === idx + 1) {
+      zhStringSum += zhTranslated[idx];
+    } else {
+      zhStringSum += zhTranslated[idx] + '\n';
+    }
+    fs.writeFileSync(zhResultPath, zhStringSum)
+  })
 
-  fs.writeFileSync(resultPath, stringSum)
   console.log('success!');
 };
