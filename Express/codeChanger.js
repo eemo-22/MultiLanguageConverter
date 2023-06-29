2a28773657557c98474b037994c4d2c7d586dcba
강호진 <eemo22@natoo.co>
2023-06-29 16:25:56 +0900

    파일에 쓰도록 설정

--- Express/codeChanger.js
+++ Express/codeChanger.js
@@ -19,12 +19,14 @@ if (templateMatch) {
   const template = templateMatch[1];
   const transformedTemplate = transformTextInTemplate(template);
 
-  // 변경된 Vue 파일 생성
+  // 변경된 Vue 코드 생성
   const transformedVueCode = vueCode.replace(template, transformedTemplate);
 
-  // <style> 영역 제거
-  const finalVueCode = transformedVueCode.replace(/<style>[\s\S]*?<\/style>/g, '');
+  // 변경된 Vue 파일의 경로 및 이름 설정
+  const outputFilePath = 'transformed-vue-file.vue';
 
-  // 변경된 Vue 파일 출력
-  console.log(finalVueCode);
+  // 변경된 Vue 파일에 쓰기
+  fs.writeFileSync(outputFilePath, transformedVueCode, 'utf-8');
+
+  console.log(`변경된 Vue 파일이 ${outputFilePath}에 성공적으로 저장되었습니다.`);
 }
